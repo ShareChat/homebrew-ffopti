1e69f1a002591a679bc976f21ddb8adc3c632536
Himanshu Dwivedi <himanshudwivedi@sharechat.co>
2022-03-31 18:01:48 +0530

    v1.0.0

--- Formula/ffopti.rb
+++ Formula/ffopti.rb
@@ -1,10 +1,10 @@
 class Ffopti < Formula
     desc "CLI tool for local assets optimization services"
     homepage "https://github.com/ShareChat/ffopti"
-    url "https://github.com/ShareChat/ffopti/releases/download/0.0.1/ffopti.tar.gz"
-    sha256 "9094a5078fb51a12f4c01cb14e653dd96721da493aecf323cb24c1b69f38cacb"
+    url "https://github.com/ShareChat/ffopti/releases/download/v1.0.0/ffopti.tar.gz"
+    sha256 "604171ff17687c022e65ff0d16a453922336e67321b5142520676a3b0232d6bb"
     license "MIT"
-    version "0.0.1"
+    version "1.0.0"
 
     depends_on "ffmpeg"
     depends_on "pngquant"
